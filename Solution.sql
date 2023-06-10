4641e924eb2dd999590c1d354813f76cd44ebe5d
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-10 22:54:04 +0530

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -1,4 +1,4 @@
-                  -- Assignment –5
+                  -- Assignment –5 and 6
  /* Relational and Logical Operators.
 1) Write a query that will give you all orders for more than Rs. 1,000.
 2) Write a query that will give you the names and cities of all salespeople in London 
@@ -66,12 +66,43 @@ select * from orders where not ((odate ='1990-10-03' or snum>1006 )and amt >=150
  -- -----------------------------------------------------------------------------------------
 
 
+             -- Assignment –6
+/* Using Special Operators in Conditions.
+1) Write two different queries that would produce all orders taken on October 3rd or 
+4
+th, 1990.
+2) Write a query that selects all of the customers serviced by Peel or Motika.
+(Hint: the snum field relates the two tables to one another). 
+3) Write a query that will produce all the customers whose names begin with a letter 
+from ‘A’ to ‘G’.
+4) Write a query that selects all customers whose names begin with the letter ‘C’.
+5) Write a query that selects all orders except those with zeroes or NULLs in the amt 
+field */
 
 
 
 
 
 
+
+select * from orders where odate = '1990-10-03';
+
+select * from orders where odate = '1990-10-04';
+
+-- -------------------------------------------------------------------
+
+select * from customers where snum =(select snum from salespeople where sname = 'Peel' or 'Motika');
+
+-- ----------------------------------------------------------------------
+-- select * from customers where cname like '[a-G]%';
+select * from customers where cname regexp '^[a-G]';
+-- -----------------------------------------------------------------------
+
+select * from customers where cname regexp '^[c]';
+-- ------------------------------------------------------------------------
+
+
+select * from orders where not amt regexp '0' or null;
 
 
 
